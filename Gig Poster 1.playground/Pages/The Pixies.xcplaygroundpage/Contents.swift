cecefb68b6d3635c4a643ea19ba2005cc56f0c9a
WilliamChan <wchan@lcs.on.ca>
2020-01-13 11:17:24 -0500

    finished text and started on white circle

--- Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
+++ Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
@@ -35,7 +35,7 @@ let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
  ## Use Source Control
  
  Remember to commit and push your work before 11:30 AM on Monday, January 13, 2020, please.
-
+ 
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
@@ -51,12 +51,58 @@ canvas.drawShapesWithFill = false
 
 for x in stride (from:0, through: 400, by: 40){
     for y in stride (from:0, through:400, by: 40){
-        canvas.drawEllipse
+        canvas.drawEllipse(at: Point(x:x, y:y), width: 40, height: 40, borderWidth: 5)
+    }
+}
+
+// draw green background
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+canvas.fillColor = limeGreen
+canvas.drawRectangle(at:Point(x:0, y:400), width: 400, height: 200, anchoredBy: AnchorPosition.bottomLeft, borderWidth: 0)
+
+//draw white circles
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+for y in stride (from: 40, through: 360, by: 40){
+    for x in stride (from:40, through:360, by:40){
+        canvas.drawEllipse(at: Point(x:x, y:y), width: 35, height: 35)
+        canvas.fillColor = offWhite
     }
 }
 
 
+// text
+// type text satudray
+canvas.drawText(message: "saturday", at: Point(x:17, y:555), size: 10, kerning: 0.0)
 
+// type text december 13 1986
+canvas.drawText(message: "december 13 1986", at: Point(x:17, y:540), size: 10, kerning: 0.0)
+
+// type text 9 pm over
+canvas.drawText(message: "9 pm over 21", at: Point(x:17, y:525), size: 10, kerning: 0.0)
+
+// type text at the rat
+canvas.drawText(message: "at the rat", at: Point(x:275, y:555), size: 10, kerning: 0.0)
+
+// type text 528 comonwealth,
+canvas.drawText(message: "528 commonwealth", at: Point(x:275, y:540), size: 10, kerning: 0.0)
+
+// type text boston, mass
+canvas.drawText(message: "boston, mass", at: Point(x:275, y:525), size: 10, kerning: 0.0)
+
+// type text pixies
+canvas.drawText(message: "pixies", at: Point(x:17, y:425), size: 68, kerning: 0.0)
+
+canvas.textColor = offWhite
+// type text with
+canvas.drawText(message: "with", at: Point(x:275, y:460), size: 10, kerning: 0.0)
+
+// type text throwing muses
+canvas.drawText(message: "throwing muses", at: Point(x:275, y: 445), size: 10, kerning: 0.0)
+
+// type text big dipper
+canvas.drawText(message: "big dipper", at: Point(x:275, y:430), size: 10, kerning: 0.0)
 
 
 
